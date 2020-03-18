c1d7d5625bcc1806c11ba2f7c994444c1ce8869f
Maxim Ryabov <foba1995@gmail.com>
2020-03-18 23:20:34 +0400

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let config = bracketsConfig.flat();
+    let result;
+    for ( let i= 0; i < config.length/2;) {
+        if (str.includes(config[i]+config[i+1])== false)
+        i+=2;
+        else{
+        str = str.replace(config[i]+config[i+1], '');
+        i = 0;
+        }
+    }
+    if (str.length==0) result = true;
+    else result = false;
+    return result;
 }
